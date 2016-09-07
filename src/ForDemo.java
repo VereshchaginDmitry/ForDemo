dee82e9ee22d489b59010540bd16f1c494a8f5e3
VereshchaginDmitry <adv.68.1@gmail.com>
2016-09-07 12:54:06 +0300

    Initial commit

---
+++ src/ForDemo.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ACCER333 on 07.09.2016.
+ */
+public class ForDemo {
+    public static void main(String[] args) {
+        int count;
+        for ( count=0 ; count < 5; count= count+1)
+        System.out.println( "Значение счетчика :" + count);
+
+        System.out.println ( "Готово !") ;
+    }
+}
